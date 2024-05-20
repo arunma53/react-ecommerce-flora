6d89f7d6d200053674a3a950e68d3a9fc1b20083
Stephy Arun <arunma53@gmail.com>
2024-05-20 14:30:16 +0000

    second

--- flora/src/App.js
+++ flora/src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, Container, Form, FormControl, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSeedling, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faSeedling, faShoppingCart ,faUser, faUserPlus} from '@fortawesome/free-solid-svg-icons';
 import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import sunflowerplant from "./sunflowerplant.jpg"
 import './style.css';
 
 function App() {
@@ -22,7 +23,7 @@ function App() {
                 <Nav className="me-auto">
                   <Nav.Link href="#home">Home</Nav.Link>
                   <Nav.Link href="#about">About</Nav.Link>
-                  <Nav.Link href="#services">Services</Nav.Link>
+                  <Nav.Link href="#services">Offers</Nav.Link>
                   <Nav.Link href="#contact">Contact</Nav.Link>
                 </Nav>
                 <Form className="d-flex mx-auto">
@@ -35,26 +36,39 @@ function App() {
                   <Button variant="outline-success">Search</Button>
                 </Form>
                 <Nav>
-                  <Nav.Link href="#login">Login</Nav.Link>
-                  <Nav.Link href="#cart">
-                    <FontAwesomeIcon icon={faShoppingCart} />
+                  <Nav.Link href="#login" >Login
+                  <FontAwesomeIcon icon={faUser} className="ms-2" />
                   </Nav.Link>
+                  <Nav.Link href="#login">Sign In
+                  <FontAwesomeIcon icon={faUserPlus} className="ms-2"/>
+                  </Nav.Link>
+
+                  {/* <Nav.Link href="#cart">
+                    <FontAwesomeIcon icon={faShoppingCart} />
+                  </Nav.Link> */}
                 </Nav>
               </Navbar.Collapse>
             </Container>
           </Navbar>
+          
         </header>
+        <Container>
+          <a href="#">
+          <img src={sunflowerplant} alt="Sunflower Plant" className="rounded-circle header-image" />
+          </a>
+        </Container>
+          
         <footer className="App-footer ">
           <Container>
-            <div className="d-flex justify-content-center py-3" >
+            <div className="d-flex justify-content-center py-3">
               <a href="https://facebook.com" className="text-white mx-3">
-                <FontAwesomeIcon icon={faFacebook} size="2x" />
+                <FontAwesomeIcon icon={faFacebook} size="2x" className="custom-icon" />
               </a>
               <a href="https://instagram.com" className="text-white mx-3">
-                <FontAwesomeIcon icon={faInstagram} size="2x" />
+                <FontAwesomeIcon icon={faInstagram} size="2x" className="custom-icon" />
               </a>
               <a href="https://twitter.com" className="text-white mx-3">
-                <FontAwesomeIcon icon={faTwitter} size="2x" />
+                <FontAwesomeIcon icon={faTwitter} size="2x" className="custom-icon" />
               </a>
             </div>
           </Container>
